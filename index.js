864e956e40b78f4c535125e88b9cf8556a6d65d4
robertfkillion@gmail.com <robertfkillion@gmail.com>
2021-06-11 09:35:11 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names,event) {
+    let messages = [];
+    for(let i=0;i<names.length;i++) {
+        messages = [...messages,`Thank you, ${names[i]}, for the wonderful ${event} gift!`];
+    }
+    return messages;
+}
+
+function countDown(num) {
+    while (num>=0) {
+        console.log(num);
+        num--;
+    }
+}
